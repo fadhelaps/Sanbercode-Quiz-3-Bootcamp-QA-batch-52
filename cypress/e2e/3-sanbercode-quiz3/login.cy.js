aa7173daaabf07372e3d22954c127c7fc9912b23
fadhelaps <aqiel727@gmail.com>
2024-01-01 21:07:39 +0700

    add custom command

--- cypress/e2e/3-sanbercode-quiz3/login.cy.js
+++ cypress/e2e/3-sanbercode-quiz3/login.cy.js
@@ -4,25 +4,37 @@ describe('login test', () => {
         cy.get('.panel > .header > .authorization-link > a').click()
     });
     
-    it('verify success login', () => {
-        cy.get('#email').type('userTest@gmail.com')
-        cy.get('#pass').type('Password_123')
+    it('verify success login', () => { //custom command
+        cy.login('userTest@gmail.com', 'Password_123')
+        // cy.get('#email').type('userTest@gmail.com')
+        // cy.get('#pass').type('Password_123')
         cy.get('#send2').click()
+    })
+
+    it('verify failed login', () => { //input wrong email
+        cy.login('userTes@gmail.com', 'Password_123')
+        // cy.get('#email').type('userTes@gmail.com')
+        // cy.get('#pass').type('Password_123')
+        cy.get('#send2').click()
+        cy.alertLoginFailed('.message-error', 'Please wait and try again later')
+        // cy.get('.message-error').should('contain', 'Please wait and try again later')
+    })
+
+    it('verify failed login', () => { //input wrong password
+        cy.login('userTes@gmail.com', 'Password_12')
+        // cy.get('#email').type('userTes@gmail.com')
+        // cy.get('#pass').type('Password_123')
+        cy.get('#send2').click()
+        cy.alertLoginFailed('.message-error', 'Please wait and try again later')
+        // cy.get('.message-error').should('contain', 'Please wait and try again later')
     })
 
     it('verify failed login', () => { //input invalid email
-        cy.get('#email').type('userTes@gmail.com')
-        cy.get('#pass').type('Password_123')
+        cy.login('1', 'Password_123')
+        // cy.get('#pass').type('Password_123')
         cy.get('#send2').click()
+        cy.emailError('#email-error', 'Please enter a valid email address')
+
     })
 
-    it('verify failed login', () => { //no email input
-        cy.get('#pass').type('Password_123')
-        cy.get('#send2').click()
-    })
-
-    it('verify failed login', () => { //no password input
-        cy.get('#email').type('userTest@gmail.com')
-        cy.get('#send2').click()
-    })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -11,7 +11,18 @@
 //
 // -- This is a parent command --
 // Cypress.Commands.add('login', (email, password) => { ... })
-//
+Cypress.Commands.add('login',(email, password) => {
+    cy.get('#email').type(email)
+    cy.get('#pass').type(password)
+})
+
+Cypress.Commands.add('alertLoginFailed', (locator, value) => {
+    cy.get(locator).should('contain.text', value)
+})
+
+Cypress.Commands.add('emailError', (locator,value) => {
+    cy.get(locator).should('contain.text', value)
+})
 //
 // -- This is a child command --
 // Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
